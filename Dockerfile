5d261c6da1760185632c57f08e6b5f440030fb34
Olivier De Meulder <odemeulder@gmail.com>
2018-11-17 17:28:29 -0500

    have server listen on port 80

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 8088
+EXPOSE 80
 RUN npm build
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get('*', (req, res) => {
   res.status(403)
 })
 
-app.listen(8088, () => {
-  console.log("Server listening on port 8888 ...")
+app.listen(80, () => {
+  console.log("Server listening on port 80 ...")
 })
